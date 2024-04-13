b7aa3bfa2df8ac3302a5266d5c265efbf20f437a
Marcel Milka <m.milka.1996@wp.pl>
2024-04-13 23:09:20 +0200

    A big chunk of ui code was moved into a composable "MyFragment".

--- app/src/main/java/com/example/h4j4/user/ui/mainComponents/MainContent.kt
+++ app/src/main/java/com/example/h4j4/user/ui/mainComponents/MainContent.kt
@@ -1,21 +1,15 @@
 package com.example.h4j4.user.ui.mainComponents
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.example.h4j4.user.ui.subsequentialComponents.MyFilterChip
-import com.example.h4j4.user.ui.subsequentialComponents.Portion
+import com.example.h4j4.user.ui.subsequentialComponents.MyFragment
 import com.example.h4j4.user.viewState.UserViewState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ColumnScope.MainContent(uiState: UserViewState) {
 
@@ -43,6 +37,7 @@ fun ColumnScope.MainContent(uiState: UserViewState) {
 
 //                    Water
                     var waterIsTracked by remember { mutableStateOf(uiState.whatIsTracked.water) }
+                    var dailyAmountOfWaterToIngest by remember { mutableStateOf(uiState.dailyAmountOfWaterToIngest.dailyAmountOfWaterToIngest.toInt()) }
 
                     var firstPortionOfWater by remember { mutableStateOf(uiState.portionsOfWater.firstPortion) }
                     var secondPortionOfWater by remember { mutableStateOf(uiState.portionsOfWater.secondPortion) }
@@ -72,6 +67,7 @@ fun ColumnScope.MainContent(uiState: UserViewState) {
 
 //                    Creatine
                     var creatineIsTracked by remember { mutableStateOf(uiState.whatIsTracked.creatine) }
+                    var dailyAmountOfCreatineToIngest by remember { mutableStateOf(uiState.dailyAmountOfCreatineToIngest.dailyAmountOfCreatineToIngest.toInt()) }
 
                     var firstPortionOfCreatine by remember { mutableStateOf(uiState.portionsOfCreatine.firstPortion) }
                     var secondPortionOfCreatine by remember { mutableStateOf(uiState.portionsOfCreatine.secondPortion) }
@@ -99,59 +95,36 @@ fun ColumnScope.MainContent(uiState: UserViewState) {
                         }
                     }
 
-                    Column(
+                    MyFragment(
+                        heightOfSurface = heightOfWaterSurface,
+                        isTracked = waterIsTracked,
+                        dailyGoal = dailyAmountOfWaterToIngest,
+                        suffix = "ml",
+                        waterOrCreatine = "Water",
+                        firstPortion = firstPortionOfWater,
+                        secondPortion = secondPortionOfWater,
+                        thirdPortion = thirdPortionOfWater,
+                        onIsTrackedChanged = { waterIsTracked = it },
+                        onFirstPortionChanged = {firstPortionOfWater = it},
+                        onSecondPortionChanged = {secondPortionOfWater = it},
+                        onThirdPortionChanged = { thirdPortionOfWater = it }
+                    )
 
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(heightOfWaterSurface.dp)
-                            .border(
-                                width = 4.dp,
-                                color = Color.White,
-                                shape = RoundedCornerShape(20.dp)
-                            )
-                            .padding(start = 10.dp),
+                    Spacer(Modifier.height(50.dp))
 
-                        horizontalAlignment = Alignment.Start,
-                        verticalArrangement = Arrangement.Top,
-
-                        content = {
-
-                            MyFilterChip(isTracked = waterIsTracked, text = "Water") {
-                                waterIsTracked = it
-                            }
-                            if (waterIsTracked) {
-
-                                Row(
-                                    Modifier.fillMaxWidth(),
-
-                                    horizontalArrangement = Arrangement.Start,
-                                    verticalAlignment = Alignment.CenterVertically,
-
-                                    content = {
-                                        Text(text ="     Daily amount of water to drink:", color = Color.White)
-                                        TextButton(
-                                            onClick = {},
-                                            content = {
-                                                Text(text = "${uiState.dailyAmountOfWaterToIngest.dailyAmountOfWaterToIngest} ml", color = Color.White)
-                                                Icon(modifier = Modifier.size(15.dp), imageVector = Icons.Rounded.Edit, contentDescription = null, tint = Color.White)
-                                            }
-                                        )
-                                    }
-                                )
-
-                                Portion(firstPortionOfWater, "ml") {newValueOfPortion: Int -> firstPortionOfWater = newValueOfPortion}
-
-                                if (firstPortionOfWater != 0) {
-
-                                    Portion(secondPortionOfWater, "ml") {newValueOfPortion: Int -> secondPortionOfWater = newValueOfPortion}
-                                }
-
-                                if (secondPortionOfWater != 0) {
-
-                                    Portion(thirdPortionOfWater, "ml") {newValueOfPortion: Int -> thirdPortionOfWater = newValueOfPortion}
-                                }
-                            }
-                        }
+                    MyFragment(
+                        heightOfSurface = heightOfCreatineSurface,
+                        isTracked = creatineIsTracked,
+                        dailyGoal = dailyAmountOfCreatineToIngest,
+                        suffix = "g",
+                        waterOrCreatine = "Creatine",
+                        firstPortion = firstPortionOfCreatine,
+                        secondPortion = secondPortionOfCreatine,
+                        thirdPortion = thirdPortionOfCreatine,
+                        onIsTrackedChanged = { creatineIsTracked = it },
+                        onFirstPortionChanged = {firstPortionOfCreatine = it},
+                        onSecondPortionChanged = {secondPortionOfCreatine = it},
+                        onThirdPortionChanged = { thirdPortionOfCreatine = it }
                     )
                 }
             }
---
+++ app/src/main/java/com/example/h4j4/user/ui/subsequentialComponents/MyFragment.kt
@@ -0,0 +1,90 @@
+package com.example.h4j4.user.ui.subsequentialComponents
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.*
+import androidx.compose.material3.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable fun MyFragment(
+    heightOfSurface: Int,
+    isTracked: Boolean,
+    dailyGoal: Int,
+    suffix: String,
+    waterOrCreatine: String,
+    firstPortion: Int,
+    secondPortion: Int,
+    thirdPortion: Int,
+    onIsTrackedChanged: (Boolean) -> Unit,
+    onFirstPortionChanged: (Int) -> Unit,
+    onSecondPortionChanged: (Int) -> Unit,
+    onThirdPortionChanged: (Int) -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(heightOfSurface.dp)
+            .border(
+                width = 4.dp,
+                color = Color.White,
+                shape = RoundedCornerShape(20.dp)
+            )
+            .padding(start = 10.dp),
+        horizontalAlignment = Alignment.Start,
+        verticalArrangement = Arrangement.Top,
+        content = {
+            MyFilterChip(isTracked = isTracked, text = waterOrCreatine) {
+                onIsTrackedChanged(it)
+            }
+            if (isTracked) {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically,
+                    content = {
+                        Text(
+                            text = "Daily amount of ${waterOrCreatine.lowercase()} to ingest:",
+                            color = Color.White
+                        )
+                        TextButton(
+                            onClick = {},
+                            content = {
+                                Text(
+                                    text = "$dailyGoal $suffix",
+                                    color = Color.White
+                                )
+                                Icon(
+                                    modifier = Modifier.size(15.dp),
+                                    imageVector = Icons.Rounded.Edit,
+                                    contentDescription = null,
+                                    tint = Color.White
+                                )
+                            }
+                        )
+                    }
+                )
+                Portion(firstPortion, suffix) { newValueOfPortion ->
+                    onFirstPortionChanged(newValueOfPortion)
+                }
+                if (firstPortion != 0) {
+                    Portion(secondPortion, suffix) { newValueOfPortion ->
+                        onSecondPortionChanged(newValueOfPortion)
+                    }
+                }
+                if (secondPortion != 0) {
+                    Portion(thirdPortion, suffix) { newValueOfPortion ->
+                        onThirdPortionChanged(newValueOfPortion)
+                    }
+                }
+            }
+        }
+    )
+}
